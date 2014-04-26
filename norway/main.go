9a43c31ea5caa1b405e9caa91ce6aeea534041ac
mulander <netprobe@gmail.com>
2014-04-26 13:42:17 +0200

    Add basic single directory entries parse on startup

--- norway/main.go
+++ norway/main.go
@@ -4,16 +4,36 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	"github.com/mulander/norway"
 )
+
+var cvs norway.Entries
+
+func Init(cvsPath string) {
+	entriesFile, err := os.Open(cvsPath + "/CVS/Entries")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer entriesFile.Close()
+
+	cvs, err = norway.ParseEntries(entriesFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	httpListen := flag.String("http", "127.0.0.1:8080", "host:port to listen on")
 	// basePath := flag.String("base", ".", "base path for web interface templates and static resources")
-	// cvsPath := flag.String("cvs", ".", "path to the cvs repository checkout")
+	cvsPath := flag.String("cvs", ".", "path to the cvs repository checkout")
 	flag.Parse()
 
 	// http.Handle("/static/", http.FileServer(http.Dir(*basePath)))
+
+	Init(*cvsPath)
 
 	if strings.HasPrefix(*httpListen, "127.0.0.1") ||
 		strings.HasPrefix(*httpListen, "localhost") {
